b1907442516e9849672d82c7266f2641e387077b
山口諒 <>
2023-01-08 00:15:55 +0900

    initial commit : useMutateAuth.ts 作成中

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import axios from 'axios'
+import { CsrfToken } from './types/types'
+import { useAppSelector } from './app/hooks'
+import { selectCsrfState } from './slices/appSlice'
+import { Auth } from './components/Auth'
+import { Todo } from './components/Todo'
+
+function App() {
+  const csrf = useAppSelector(selectCsrfState)
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const res = await axios.get<CsrfToken>(
+        `${process.env.REACT_APP_API_URL}/csrftoken`
+      )
+      axios.defaults.headers.common['X-CSRF-Token'] = res.data.csrf_token
+    }
+    getCsrfToken()
+  }, [csrf])
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Auth />
+        </Route>
+        <Route exact path="/todo">
+          <Todo />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import App from './App'
+import './index.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
